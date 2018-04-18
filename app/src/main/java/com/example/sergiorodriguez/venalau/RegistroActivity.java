3e91ac3b03ab36a7e11d3d08bf708d202b14ae43
marianamendivil <mariana.mendidu@gmail.com>
2018-04-18 15:31:58 -0500

    mariana
    registro
    implementos

--- app/src/main/java/com/example/sergiorodriguez/venalau/RegistroActivity.java
+++ app/src/main/java/com/example/sergiorodriguez/venalau/RegistroActivity.java
@@ -2,6 +2,8 @@ package com.example.sergiorodriguez.venalau;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+
+    public void OnClickBotonRegistrar(View v) {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
     }
 }
